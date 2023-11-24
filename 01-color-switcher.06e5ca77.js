4f91e48115ff528519d7347b45796a75f12a9617
TinaDavy <107054810+TinaDavy@users.noreply.github.com>
2023-11-24 22:20:30 +0000

    Deploying to gh-pages from @ TinaDavy/goit-js-hw-09@be78b04c574bbf66a8850f9474549f213b433bca 🚀

--- 01-color-switcher.06e5ca77.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.disabled=!0,e.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.06e5ca77.js.map
