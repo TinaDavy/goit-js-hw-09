ba9d1178433348df40e82b4b109bfeef9ee21366
taniadavy <tania111davidenko111@gmail.com>
2023-11-24 23:07:11 +0100

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+startBtn.addEventListener("click", onClick);
+
+stopBtn.disabled = true;
+
+function onClick(){
+    timerId = setInterval(() => {
+        body.style.backgroundColor =  getRandomHexColor();
+    }, 1000)
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+};
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
 
