336432334e60c34a3278f56ebbf6114785f32930
TinaDavy <107054810+TinaDavy@users.noreply.github.com>
2023-11-24 22:07:34 +0000

    Deploying to gh-pages from @ TinaDavy/goit-js-hw-09@ba9d1178433348df40e82b4b109bfeef9ee21366 🚀

---
+++ 01-color-switcher.d17b2c1c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.disabled=!0,e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.d17b2c1c.js.map
